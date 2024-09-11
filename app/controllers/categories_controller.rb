b992ca54832e41b4f7fcce0e75b108a405b5675f
Xhanti Mahonga <xhantis@live.co.za>
2024-09-11 15:26:03 +0200

    creating controllers and views for categories and homepage

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,33 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])  
+
+    @topics = @category.topics
+  end
+
+  def new
+    @category = Category.new(user: current_user)
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    @category.user = current_user unless @category.user
+
+    if @category.save
+
+      redirect_to @category, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+    private
+
+    def category_params
+      params.require(:category).permit(:title, :description)
+    end
 end
