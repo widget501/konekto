f6c722dd3605ed3e776a5c50770352131d74be84
JP Breugem <36500401+widget501@users.noreply.github.com>
2024-09-11 15:24:11 +0200

    Modify routes; add Topics controller and views

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,48 @@
 class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    if @topic.save
+      redirect_to @topic, notice: 'Topic created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update(topic_params)
+      redirect_to @topic, notice: 'Topic updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to topics_url, notice: 'Topic deleted.'
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title, :description, :category_id, :user_id)
+  end
 end
