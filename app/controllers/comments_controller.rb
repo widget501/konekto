f0a1bc9d6067f5c0a230f268349529c151034a79
JP Breugem <36500401+widget501@users.noreply.github.com>
2024-09-13 13:04:30 +0200

    Modified routes, cntr's & views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,19 +5,17 @@ class CommentsController < ApplicationController
 
   def new
     @post = Post.find(params[:post_id])
-    @comment.post = @post
-    @comment.user = current_user
     @comment = Comment.new
   end
 
   def create
     @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
     @comment.post = @post
     @comment.user = current_user
-    @comment = Comment.new(comment_params)
 
     if @comment.save
-      redirect_to @comment, notice: 'Comment created.'
+      redirect_to post_path(@post), notice: 'Comment created.'
     else
       render :new
     end
@@ -26,6 +24,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id)
+    params.require(:comment).permit(:content)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :topics
+  validates :title, uniqueness: true
 end
