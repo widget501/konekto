c666c16d899e05ddf925fc42731702e129c8b01b
Xhanti Mahonga <xhantis@live.co.za>
2024-09-17 10:57:25 +0200

    scrolling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,22 +4,41 @@ import "controllers"
 import "@popperjs/core"
 import "bootstrap"
 
-const autoScrollDiv = (scrollContainerSelector, speed = 1) => {
-  const scrollContainer = document.querySelector(scrollContainerSelector);
+// const autoScrollDiv = (scrollContainerSelector, speed = 1) => {
+//   const scrollContainer = document.querySelector(scrollContainerSelector);
+
+//   if (!scrollContainer) return;
+
+//   let scrollPosition = 0;
+
+//   const scrollInterval = setInterval(() => {
+//     scrollPosition += speed
+//     scrollContainer.scrollTop += speed
+//     if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight) {
+//       clearInterval(scrollInterval);
+//     }
+//   }, 20);
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   autoScrollDiv('.scrollable-container', 1);
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scrollContainer = document.querySelector(".scrollable-container");
 
   if (!scrollContainer) return;
 
-  let scrollPosition = 0;
+  const speed = 1.2;
 
-  const scrollInterval = setInterval(() => {
-    scrollPosition += speed
+
+  const autoScroll = () => {
     scrollContainer.scrollTop += speed
+    console.log("Big Nyash")
     if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight) {
-      clearInterval(scrollInterval);
+      scrollContainer.scrollTop = 0
     }
-  }, 20);
-}
+  };
 
-document.addEventListener('DOMContentLoaded', () => {
-  autoScrollDiv('.scrollable-container', 1);
+  setInterval(autoScroll, 50);
 });
